81051d2cb845fba07f463ff1a39b05fda5e5de46
mlinhpe <mailinh.pham711@gmail.com>
2020-12-13 13:14:10 +0100

    update

--- Mail.js
+++ Mail.js
@@ -1,8 +1,10 @@
 const express = require("express")
-const port=10011
+const port= 10011
 const app = express()
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false});
+const PDFDocument = require('pdfkit')
+const fs = require('fs')
 
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
@@ -15,14 +17,12 @@ var transporter = nodemailer.createTransport({
 
 
 app.post('/mail', urlencodedParser, (req, res) => {
-    const currentDate = new Date()
-    const name = req.body.name
     const email = req.body.email
     const subject = req.body.subject
     const message = req.body.message
 
     var mailOptions = {
-        from: name,
+        from: email,
         to: 'pwptesterml@gmail.com',
         subject: subject,
         text: message
@@ -39,5 +39,52 @@ app.post('/mail', urlencodedParser, (req, res) => {
       });
 
 })
+
+const resumeText =
+`
+Resume
+
+Mai Linh Pham
+mai_linh.pham@campus.lmu.de
+
+
+- Education
+
+University
+-- B.Sc. Business Administration, LMU (2014)
+-- B.Sc. Computational Linguistics, LMU (2017)
+-- M.Sc. Computer Science, LMU (tba)
+
+
+- Work
+
+EQS Group AG
+-- Working Student, Backend Developer (May 2017 - Sept. 2017)
+
+gutefrage.net
+-- Working Student, Data Engineer (Oct. 2017 - Mar. 2018)
+
+ProsiebenSat1 Tech-Solutions GmbH
+-- Data Engineer (Apr. 2018 - now)
+
+
+- Skills
+
+Scala: 50%
+Python | NLP: 20%
+SQL: 45%
+Java: 25%
+JavaScript | CSS | HTML: 10%
+
+`
+
+app.get('/download-cv', (req, res) => {
+    let pdfDoc = new PDFDocument;
+    pdfDoc.pipe(fs.createWriteStream('Mailinh_Pham_CV.pdf')).on("finish", function() {
+        res.download("Mailinh_Pham_CV.pdf");
+    });
+    pdfDoc.text(resumeText)
+    pdfDoc.end();
+});
 
 app.listen(port)
